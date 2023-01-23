a600e3b899e9ecacca421004bd5b4315ab9ab198
timf34 <farrelti@tcd.ie>
2023-01-23 16:18:57 +0000

    homography logic class implemented and working

--- homgraphy_logic.py
+++ homgraphy_logic.py
@@ -2,28 +2,98 @@ import cv2
 import os
 import numpy as np
 
-from typing import NamedTuple, Tuple, List, Dict
+from typing import NamedTuple, Tuple, List, Dict, Union
 
 from config import CameraJetson3, CameraJetson1, RealWorldPitchCoords, HOMOGRAPHY_SAVE_DIR
 from utils import save_np_matrix
 
 
 class HomographyLogic:
-    def __init__(self):
-        pass
+    def __init__(self, convert_to_pixels: bool = False):
+        self.jetson1 = CameraJetson1()
+        self.jetson3 = CameraJetson3()
+        self.real_world_pitch_coords = RealWorldPitchCoords()
+        if convert_to_pixels:
+            self.real_world_pitch_coords.convert_to_pixel_coords()  # Instead of to metres
+        self.homographies: Dict[str, np.ndarray] = self.get_homographies()
+
+    def get_real_world_coords(self, jetson_camera: Union[CameraJetson3, CameraJetson1]) -> Tuple[np.ndarray, np.ndarray]:
+        """
+        Converts jetson camera coords + real world coords to np.ndarray. It also skips None values.
+        :param jetson_camera:
+        :return:
+        """
+        # Initialize two empty arrays to store the pixel coords
+        camera_coords: np.ndarray = np.array([])
+        world_points = np.array([])
+
+        for key in jetson_camera.__dict__.keys():
+            if key in self.real_world_pitch_coords.__dict__.keys() and self.real_world_pitch_coords.__dict__[key] is not \
+                    None and jetson_camera.__dict__[key] is not None:
+                camera_coords = np.append(camera_coords, jetson_camera.__dict__[key], axis=0)
+                world_points = np.append(world_points, self.real_world_pitch_coords.__dict__[key])
+
+        camera_coords = camera_coords.reshape(-1, 2)
+        world_points = world_points.reshape(-1, 2)
+
+        assert len(camera_coords) == len(world_points), "The number of points in the camera array doesn't equal the real world array"
+        assert camera_coords.shape[1] == 2, "The camera coords array should have 2 columns (be 2D)"
+
+        return camera_coords, world_points
+
+    def find_homography(self, camera_coords: np.ndarray, real_world_coords: np.ndarray) -> np.ndarray:
+        """
+        Computes the homography matrix for a given camera and real world coords
+        :param jetson_camera:
+        :param real_world_coords:
+        :return: h: the homography matrix
+        """
+        h, status = cv2.findHomography(camera_coords, real_world_coords)
+
+        return h
+
+    def get_homographies(self) -> Dict[str, np.ndarray]:
+        c1, r1 = self.get_real_world_coords(jetson_camera=self.jetson1)
+        c3, r3 = self.get_real_world_coords(jetson_camera=self.jetson3)
+
+        h1 = self.find_homography(camera_coords=c1, real_world_coords=r1)
+        h3 = self.find_homography(camera_coords=c3, real_world_coords=r3)
+
+        return {"1": h1, "3": h3}
+
+
+# def peform_homography(homography: np.ndarray, point: np.ndarray) -> np.ndarray:
+#     transformed_point = homography @ point
+#     return transformed_point / transformed_point[2]
+
+
+def inspect_shape_homography(homo: np.ndarray) -> None:
+    # # Transform the point
+    test_point = np.array([[1408], [310], [1.0]], dtype='float32')
+
+    transformed_point = homo @ test_point  # This isn't super accurate but we'll use it just to move on for now.
+    transformed_point = transformed_point / transformed_point[2]
+    print("1: ", transformed_point)
+
+    # Test with h1
+    test_point = np.array([[1062], [817], [1.0]], dtype='float32')
+    transformed_point = homo @ test_point  # This isn't super accurate but we'll use it just to move on for now.
+    transformed_point = transformed_point / transformed_point[2]
+    print("2: ", transformed_point)
+
+    assert transformed_point.shape == (3, 1), "The transformed point is not 3x1"
 
 
 def compute_homographies(file_name: str = "new_h_jetson") -> Dict[str, np.ndarray]:
     jetson1 = CameraJetson1()
     jetson3 = CameraJetson3()
     real_world = RealWorldPitchCoords()
-    real_world.convert_to_pixel_coords()
+    # real_world.convert_to_pixel_coords()
     print(real_world.__dict__)
 
     # Create an empty np array to store the pixel coordinates
     j1_arr = np.array([])
     j2_arr = np.array([])
-
 
     # Get the pixel coordinates from the jetson cameras, and the real world coordinates. Convert to NP.arrays
     world_points1 = np.array([])
@@ -116,7 +186,30 @@ def inspect_homography():
 
 
 def main():
-    inspect_homography()
+    # inspect_homography()
+    # Old function
+    old_h = compute_homographies()
+
+    # New class
+    h_logic = HomographyLogic()
+    new_h = h_logic.get_homographies()
+
+    old_h1 = old_h["1"]
+    print("old_h1: ", old_h1)
+    old_h3 = old_h["3"]
+
+    new_h1 = new_h["1"]
+    print("new_h1", new_h1)
+    new_h3 = new_h["3"]
+
+    print(f"Old h1 shape {old_h1.shape}")
+    print(f"New h1 shape {new_h1.shape}")
+
+    print(f"Old h3 shape {old_h3.shape}")
+    print(f"New h3 shape {new_h3.shape}")
+
+    print(f"Old h1 == new h1: {np.array_equal(old_h1, new_h1)}")
+    print(f"Old h3 == new h3: {np.array_equal(old_h3, new_h3)}")
 
 
 if __name__ == '__main__':
