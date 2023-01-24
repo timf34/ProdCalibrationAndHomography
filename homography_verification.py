bb981964c602e18512ae64acc20b6bfd40277afa
timf34 <farrelti@tcd.ie>
2023-01-24 13:10:11 +0000

    cleaned up considerably - atomised functions reasonably well

--- homography_verification.py
+++ homography_verification.py
@@ -1,13 +1,12 @@
 import cv2
-import json
 import pandas as pd
 import numpy as np
-from typing import Dict, List, Iterator
+from typing import Iterator
 
-from config import CameraJetson3, CameraJetson1, RealWorldPitchCoords
 from homgraphy_logic import HomographyLogic
 
 np.set_printoptions(suppress=True)  # Suppress scientific notation
+pd.options.display.max_columns = None  # Display all pandas columns
 
 
 class VisualizeHomography:
@@ -35,6 +34,8 @@ class VisualizeHomography:
     def get_transformed_points(self, camera_points: np.ndarray) -> np.ndarray:
         """
         Transforms the camera points to the real world points.
+        :param camera_points: The camera points to transform - shape is (n, 2)
+        :return: The transformed points - shape is (n, 2)
         """
         camera_points_3d = np.c_[camera_points, np.ones(len(camera_points))]
         transformed_points = np.array([])
@@ -46,49 +47,19 @@ class VisualizeHomography:
 
         return transformed_points.reshape(-1, 3)[:, :-1]  # Reshape to (n, 3) and remove the last column (all 1's)
 
-
-    def check_transformed_points(self) -> None:
+    def print_stats(self, homography_dataframe: pd.DataFrame) -> None:
         """
-        Perform a homography on our original points and compare them to the transformed points.
-        Ensure that the transformed points are the same as the original points, or very close to them.
+        This function will print the stats of the homography dataframe -> Difference, mean, std dev
+        :param homography_dataframe: Columns are "key", "transformed_point", "difference", "real_world", "camera_coords"
         """
-        # Get the original points
-        c3, r3 = self.hl.get_real_world_coords(jetson_camera=self.hl.jetson3)
+        # print("Difference between original and transformed points: ", homography_dataframe["difference"].values)
+        # print("Mean difference between original and transformed points: ", np.mean(homography_dataframe["difference"].values, axis=0))
+        # print("Standard deviation of the difference between original and transformed points: ", np.std(homography_dataframe["difference"].values, axis=0))
 
-        print("yo2, £, ", type(c3))
-        print("c3 shape: ", c3.shape)
-        transformed_points = self.get_transformed_points(camera_points=c3)
-
-        # Copy numpy array to iterator to avoid changing original array
-        transformed_points_iterator = iter(transformed_points.copy())
-
-
-        # Compare the original points to the transformed points
-        print("Original points: ", c3)
-        print("real world points (targets): ", r3)
-        print("Transformed points: ", transformed_points)
-
-        # # Calculate the difference between the original and transformed points
-        diff = r3 - transformed_points
-        # Round all points to 1 decimal place
-        diff = np.round(diff, 1)
-        print("Difference between original and transformed points: ", diff)
-
-        # Calculate the mean difference between the original and transformed points
-        mean_diff = np.mean(diff, axis=0)
-        print("Mean difference between original and transformed points: ", mean_diff)
-
-        # Calculate the standard deviation of the difference between the original and transformed points
-        std_diff = np.std(diff, axis=0)
-        print("Standard deviation of the difference between original and transformed points: ", std_diff)
-
-        # Creating another dictionary to print here so we can clearly identify points.
-        iter_diff = iter(diff)
-        labelling_points = self.get_labelled_dict(iter_diff, transformed_points_iterator, real_world_points=iter(r3), camera_points=iter(c3))
-
-        # Print everything in the pd dataframe labelling_points
-        pd.options.display.max_columns = None
-        print(labelling_points)
+        # We first need to convert the diff column to a np array
+        diff = np.array([np.array(x) for x in homography_dataframe["difference"].values])
+        print("Mean difference between original and transformed points: ", np.mean(diff, axis=0))
+        print("Standard deviation of the difference between original and transformed points: ", np.std(diff, axis=0))
 
     def get_labelled_dict(self, iter_diff: Iterator, transformed_points_iterator: Iterator, real_world_points: Iterator,
                           camera_points: Iterator) -> pd.DataFrame:
@@ -97,13 +68,33 @@ class VisualizeHomography:
         """
         # Create pandas dataframe
         keys = [key for key in self.hl.jetson3.__dict__ if self.hl.jetson3.__dict__[key] is not None]
-        df = pd.DataFrame(columns=["key", "transformed_point", "difference", "real_world", "camera"])
+        df = pd.DataFrame(columns=["key", "transformed_point", "difference", "real_world", "camera_coords"])
         for count, (key, j, _diff, real, cam_p) in enumerate(zip(keys, transformed_points_iterator, iter_diff,
                                                                  real_world_points, camera_points)):
             if key is not None:
                 df.loc[count] = [key, j.tolist(), _diff.tolist(), real, cam_p]
 
         return df
+
+    def check_transformed_points(self) -> None:
+        """
+        Perform a homography on our original points and compare them to the transformed points.
+        Ensure that the transformed points are the same as the original points, or very close to them.
+        """
+        # Get the original points
+        c3, r3 = self.hl.get_real_world_coords(jetson_camera=self.hl.jetson3)
+        # Get the transformed points
+        transformed_points = self.get_transformed_points(camera_points=c3)
+
+        diff = r3 - transformed_points  # Calculate the difference between the original and transformed points
+        diff = np.round(diff, 1)  # Round to 1 decimal place
+
+        labelling_points = self.get_labelled_dict(iter(diff), iter(transformed_points.copy()),
+                                                  real_world_points=iter(r3), camera_points=iter(c3))
+
+        self.print_stats(labelling_points)
+
+        print(labelling_points)
 
 
 def main():
